c5a0999f16632ff67ccdbe8923b64df53cb8db64
senwa105 <senwa105@gmail.com>
2020-10-04 16:21:57 -0400

    Fixed cutoff feature in search_algorithms

--- search_algorithms.py
+++ search_algorithms.py
@@ -155,7 +155,7 @@ class DepthFirstSearch(GoalSearchAgent):
     def enqueue(self, state: StateNode, cutoff: Union[int, float] = INF):
         """ Add the state to the frontier, unless depth exceeds the cutoff """
         # TODO 
-        if state.depth < cutoff:
+        if not state.depth > cutoff:
             self.frontier.append(state)
         
     def dequeue(self) -> StateNode:
@@ -187,7 +187,7 @@ class BreadthFirstSearch(GoalSearchAgent):
         
     def enqueue(self, state: StateNode, cutoff: Union[int, float] = INF):
         """ Add the state to the frontier, unless depth exceeds the cutoff """
-        if not state.depth>cutoff: #state.depth>=cutoff
+        if not state.depth > cutoff: #state.depth>=cutoff
             self.frontier.append(state)
         
 
@@ -230,7 +230,7 @@ class UniformCostSearch(GoalSearchAgent):
     def enqueue(self, state: StateNode, cutoff: Union[int, float] = INF):
         """ Add the state to the frontier, unless path COST exceeds the cutoff """
         # TODO 
-        if state.path_cost <= cutoff:
+        if not state.path_cost > cutoff:
             heapq.heappush(self.frontier, (state.path_cost, state))
 
         

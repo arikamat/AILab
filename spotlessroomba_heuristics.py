34ffb367deed2d8046c7b88a1f1bb6e266e2aa8f
senwa105 <senwa105@gmail.com>
2020-10-19 00:15:49 -0400

    attempted to implement spotlessroomba heuristics

--- spotlessroomba_heuristics.py
+++ spotlessroomba_heuristics.py
@@ -17,24 +17,61 @@ def spotlessroomba_first_heuristic(state : SpotlessRoombaState)  -> float:
     h = 0
     current_position = state.position
     dirty_locations = list(state.dirty_locations)
-    partial_heuristic = INF
+    lowest = INF
     closest_dirty = 0
 
-    while dirty_locations:
+    """for count in range(2):
+        if not dirty_locations:
+            return h
         for i in range(len(dirty_locations)):
             manhattan = abs(current_position.row - dirty_locations[i].row) + abs(current_position.col - dirty_locations[i].col)
-            if manhattan < partial_heuristic:
-                partial_heuristic = manhattan
+            if manhattan < lowest:
+                lowest = manhattan
                 closest_dirty = i
-        h += partial_heuristic
+        h += lowest
         current_position = dirty_locations.pop(closest_dirty)
-        partial_heuristic = INF
-        
-    return h
+        lowest = INF"""
+
+    for i in range(len(dirty_locations)):
+            manhattan = abs(current_position.row - dirty_locations[i].row) + abs(current_position.col - dirty_locations[i].col)
+            lowest = min(lowest, manhattan)
+            
+    return lowest
 
 def spotlessroomba_second_heuristic(state : SpotlessRoombaState)  -> float:
     # TODO a nontrivial consistent heuristic
-    raise NotImplementedError
+    best_path = [] # shortest order to visti all dirty tiles
+    best_cost = INF # cost of above path
+
+    # find shortest sequence of cleaning the dirty tiles
+    for i in range(len(state.dirty_locations)):
+        estimated_cost = 0
+        lowest = INF
+        closest_dirty = 0
+        dirty_locations = list(state.dirty_locations)
+        # set the start node and remove it from the list of nodes that have to be traveled to
+        current_position = dirty_locations.pop(i)
+        path = [current_position]
+        
+        while dirty_locations:
+            for j in range(len(dirty_locations)):
+                manhattan = abs(current_position.row - dirty_locations[j].row) + abs(current_position.col - dirty_locations[j].col)
+                if manhattan < lowest:
+                    lowest = manhattan
+                    closest_dirty = j
+            estimated_cost += lowest
+            current_position = dirty_locations.pop(closest_dirty)
+            path.append(current_position)
+            lowest = INF
+        if best_cost > estimated_cost:
+            best_cost = estimated_cost
+            best_path = path
+
+    current_position = state.position
+    # manhattan distance to next node in best_path
+    manhattan_to_next_dirty = abs(current_position.row - best_path[0].row) + abs(current_position.row - best_path[0].col)
+    return best_cost + manhattan_to_next_dirty
+
 
 # Make sure to update names below, and add any extra you create.
 SPOTLESSROOMBA_HEURISTICS = {"Zero" : zero_heuristic,
